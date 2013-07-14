f2868ebb6d103e7613f422dd396635ac65a04ced
Kenneth Belitzky <kenny@belitzky.com>
2013-07-13 22:47:23 -0300

    separate collected standalone and apache config

--- recipes/collectd_web_apache.rb
+++ recipes/collectd_web_apache.rb
@@ -18,30 +18,14 @@
 #
 
 include_recipe "collectd"
+include_recipe "apache2"
+include_recipe "collectd_web"
 
-%w(libhtml-parser-perl liburi-perl librrds-perl libjson-perl).each do |name|
-  package name
-end
-
-directory node[:collectd][:collectd_web][:path] do
+template "/etc/apache2/sites-available/collectd_web.conf" do
+  source "collectd_web.conf.erb"
   owner "root"
   group "root"
-  mode "755"
-
-  not_if do
-    File.directory? node[:collectd][:collectd_web][:path]
-  end
+  mode "644"
 end
 
-bash "install_collectd_web" do
-  user "root"
-  cwd node[:collectd][:collectd_web][:path]
-  not_if do
-    File.exists?(File.join(node[:collectd][:collectd_web][:path], "index.html"))
-  end
-  code <<-EOH
-    wget --no-check-certificate -O collectd-web.tar.gz https://github.com/httpdss/collectd-web/tarball/master
-    tar --strip-components=1 -xzf collectd-web.tar.gz
-    rm collectd-web.tar.gz
-  EOH
-end
+apache_site "collectd_web.conf"
